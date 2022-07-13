0e732a53506809e538ff8248d39483f060334b8d
yihailin <1161813899@qq.com>
2022-07-14 00:18:39 +0800

    finish os4 sys_get_time, sys_task_info

---
+++ os4/src/syscall/process.rs
@@ -0,0 +1,74 @@
+//! Process management syscalls
+
+use crate::config::MAX_SYSCALL_NUM;
+use crate::task::{get_task_info, exit_current_and_run_next, suspend_current_and_run_next,current_user_token, TaskStatus};
+use crate::timer::get_time_us;
+use crate::mm::{get_phyaddr};
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct TimeVal {
+    pub sec: usize,
+    pub usec: usize,
+}
+
+#[derive(Clone, Copy)]
+pub struct TaskInfo {
+    pub status: TaskStatus,
+    pub syscall_times: [u32; MAX_SYSCALL_NUM],
+    pub time: usize,
+}
+
+pub fn sys_exit(exit_code: i32) -> ! {
+    info!("[kernel] Application exited with code {}", exit_code);
+    exit_current_and_run_next();
+    panic!("Unreachable in sys_exit!");
+}
+
+/// current task gives up resources for other tasks
+pub fn sys_yield() -> isize {
+    suspend_current_and_run_next();
+    0
+}
+
+// YOUR JOB: 引入虚地址后重写 sys_get_time
+pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
+    let _us = get_time_us();
+    let ts = get_phyaddr(current_user_token(), _ts as usize) as *mut TimeVal;
+    
+    unsafe {
+        *ts = TimeVal {
+            sec: _us / 1_000_000,
+            usec: _us % 1_000_000,
+        };
+    }
+    0
+}
+
+// CLUE: 从 ch4 开始不再对调度算法进行测试~
+pub fn sys_set_priority(_prio: isize) -> isize {
+    -1
+}
+
+// YOUR JOB: 扩展内核以实现 sys_mmap 和 sys_munmap
+pub fn sys_mmap(_start: usize, _len: usize, _port: usize) -> isize {
+    -1
+}
+
+pub fn sys_munmap(_start: usize, _len: usize) -> isize {
+    -1
+}
+
+// YOUR JOB: 引入虚地址后重写 sys_task_info
+pub fn sys_task_info(ti: *mut TaskInfo) -> isize {
+    let ts = get_phyaddr(current_user_token(), ti as usize) as *mut TaskInfo;
+    let (syscall_times, time) = get_task_info();
+    unsafe {
+        *ts = TaskInfo{
+            status: TaskStatus::Running,
+            syscall_times: syscall_times,
+            time: time
+        }
+    }   
+    0
+}
